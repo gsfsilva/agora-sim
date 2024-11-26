de0fb9aa2a13dbbb73fdc03854ee51c259e94f11
Gustavo Silva <97358905+gsfsilva@users.noreply.github.com>
2024-11-25 23:52:25 -0300

    Add files via upload

---
+++ SITE_PHP/view/sobre.php
@@ -0,0 +1,186 @@
+<?php include("blades/header.php");?>
+<link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="css/style.css">
+<title>Women's Forum</title>
+</head>
+
+<body>
+    <main>
+        <section class="home">
+            <div class="home-text">
+                <h4 class="text-h4">Bem vindo ao <span>W</span>omen<span>'s</span> <span>F</span>orum</h4>
+                <h1 class="text-h2"><span>Conheça</span> nossas <span> plataformas </span></h1>
+                <p>"A solução para acabar com a violencia domestica em forma de aplicativos"
+                </p>
+            </div>
+            <div class="home-img">
+                <img src="../img/telas1.png" alt="mulher">
+            </div>
+        </section>
+    </main>
+
+    <section class="sobre">
+        <div class="interface">
+            <div class="flex">
+                <div class="img-sobre">
+                    <img src="../img/tela1.png" alt="casa-mulher">
+                </div>
+                <!--img-sobre-->
+
+                <div id="destino" class="txt-sobre">
+                    <h2>Nosso<span> Aplicativo</span></h2>
+                    <p>O nosso aplicativo foi desenvolvido com funcionalidades para ajudar as mulheres a terem mais
+                        facilidade para encontrar as casas de abrigo
+                        mais próximas de sua localização e também na entrada das mulheres nas casas de abrigo, as quais
+                        estão com problemas de
+                        violência doméstica. O aplicativo tem como objetivo ajudar a mulher a se sentir mais segura e a
+                        ter mais facilidade para entrar nas casas de abrigo.</p>
+                    </p>
+                    <p>O App tem como medida de segurança o requerimento da foto da face da mulher para garantir que só
+                        mulheres se cadastrem e façam as solicitações,
+                        com intuito de facilitar o acesso a essas casas.</p>
+                </div>
+                <!--txt-sobre-->
+            </div>
+            <!--flex-->
+        </div>
+        <!--interface-->
+    </section>
+    <!--sobre-->
+
+    <section class="meio-do-site">
+        <div class="interface">
+            <div class="flex">
+                <div class="txt-meio-site">
+                    <h1>Nosso<span> Desktop</span></h1>
+                    <p>
+                        O desktop foi desenvolvido para os gerenciadores das casas de abrigo, onde eles podem visualizar
+                        as solicitações feitas pelas mulheres e aceitar ou recusar a entrada delas nas casas de abrigo.
+                        O sistema foi feito para facilitar o acesso dos gerenciadores as solicitações e também para que
+                        eles possam ter um controle maior sobre as solicitações feitas pelas mulheres. Isso faz com que
+                        tenha mais segurança
+                        e controle em quem vai ser enviada a essas casas.
+                    </p>
+                </div>
+                <!--txt-meio-site-->
+
+                <div class="img-meio-site">
+                    <img src="../img/tela2.png" alt="">
+                </div>
+                <!--img-meio-site-->
+            </div>
+            <!--flex-->
+        </div>
+        <!--interface-->
+    </section>
+    <section class="meio-do-site">
+        <div class="interface">
+            <div class="flex">
+                <class="sobre-nos">
+                    <h1>Desenvolvedores</h1>
+            </div>
+            <div class="cards">
+                <div class="center">
+                    <div class="property-card">
+                        <a href="#">
+                            <div class="property-image">
+                            <img src="../img/ana.jpg" alt="mulher">
+                                <div class="property-image-title">
+
+                                </div>
+                            </div>
+                        </a>
+                        <div class="property-description">
+                            <h5>Ana Flávia</h5>
+                            <p>Web developer <br>
+                                UX/UI Designer</p>
+                        </div>
+                        <a href="#">
+                            <div class="property-social-icons">
+                                <!-- I would usually put multipe divs inside here set to flex. Some people might use Ul li. Multiple Solutions -->
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="center">
+                    <div class="property-card">
+                        <a href="#">
+                            <div class="property-image">
+                            <img src="../img/gusta.jpg" alt="mulher">
+                                <div class="property-image-title">
+
+                                </div>
+                            </div>
+                        </a>
+                        <div class="property-description">
+                            <h5> Gustavo Silva </h5>
+                            <p>Web developer <br> UX/UI Designer</p>
+                        </div>
+                        <a href="#">
+                            <div class="property-social-icons">
+                                <!-- I would usually put multipe divs inside here set to flex. Some people might use Ul li. Multiple Solutions -->
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="center">
+                    <div class="property-card">
+                        <a href="#">
+                            <div class="property-image">
+                            <img src="../img/anak.jpg" alt="mulher">
+                                <div class="property-image-title">
+
+                                </div>
+                            </div>
+                        </a>
+                        <div class="property-description">
+                            <h5> Ana Mascarenhas </h5>
+                            <p>Redator<br>
+                                Desktop <br> UX/UI Designer</p>
+                        </div>
+                        <a href="#">
+                            <div class="property-social-icons">
+                                <!-- I would usually put multipe divs inside here set to flex. Some people might use Ul li. Multiple Solutions -->
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="center">
+                    <div class="property-card">
+                        <a href="#">
+                            <div class="property-image">
+                            <img src="../img/ge.jpg" alt="mulher">  
+                                <div class="property-image-title">
+                                </div>
+                            </div>
+                        </a>
+                        <div class="property-description">
+                            <h5> Geovana </h5>
+                            <p>Mobile developer <br>
+                                UX/UI Designer</p>
+                        </div>
+                        <a href="#">
+                            <div class="property-social-icons">
+                                <!-- I would usually put multipe divs inside here set to flex. Some people might use Ul li. Multiple Solutions -->
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    </section>
+
+
+
+
+    <script src="script.js"></script>
+</body>
+
+</html>
+
+<?php include("blades/footer.php");?>
